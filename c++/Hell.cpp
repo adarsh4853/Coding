71e814138e33fcde8fdae1c009aab69619e40941
adarsh4853 <154544189+adarsh4853@users.noreply.github.com>
2024-02-16 15:32:53 +0530

    Change

---
+++ c++/Hell.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, flag = 0, j;
+    cin >> n;
+    for (int i = 0;; i++)
+    {
+        ++n;
+        flag = 1;
+        for (j = 2; j < n; j++)
+        {
+            if (n % j == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+            break;
+    }
+    cout << j<< '\n';
+    return 0;
+}
